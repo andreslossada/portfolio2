99f79fdc0bd8a1b17cf96c566dd97e316dd4fc8a
dresan22 <andresat48@hotmail.com>
2024-05-06 13:33:16 -0400

    fix hover glitch with bento

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -66,6 +66,17 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      dropShadow: {
+        orange: "0px 0px 15px #f59e0b",
+        "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
+        "4xl": [
+          "0 35px 35px rgba(0, 0, 0, 0.25)",
+          "0 45px 65px rgba(0, 0, 0, 0.15)",
+        ],
+      },
+      shadow: {
+        orange: "0px 0px 15px #f59e0b",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
