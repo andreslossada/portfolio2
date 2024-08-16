f281acac7f8d8d4dc5d5eb46bdf9c4b5fd1fcbed
dresan22 <andresat48@hotmail.com>
2024-08-16 12:50:31 -0400

    hardcoded color objest

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,22 +1,32 @@
 var word = "andres";
-var colorObj = {};
+var colorObj = {
+  primary: "#f92672",
+  secondary: "#61aeee",
+  tertiary: "#98c379",
+  forth: "#e6c07b",
+  // dark: "#0a0d1e",
+  // light: "#344078",
+  // muted: "#7f7f7f",
+};
 
-function rgbToHex(rgb) {
-  var a = rgb.split("(")[1].split(")")[0];
-  a = a.split(",");
-  var b = a.map(function (x) {
-    x = parseInt(x).toString(16);
-    return x.length == 1 ? "0" + x : x;
-  });
-  return "#" + b.join("");
-}
+// function rgbToHex(rgb) {
+//   var a = rgb.split("(")[1].split(")")[0];
+//   a = a.split(",");
+//   var b = a.map(function (x) {
+//     x = parseInt(x).toString(16);
+//     return x.length == 1 ? "0" + x : x;
+//   });
+//   return "#" + b.join("");
+// }
 
-for (var i = 0; i < word.length; i++) {
-  var letter = word[i];
-  var element = document.querySelector(".letter-" + letter);
-  var style = window.getComputedStyle(element, null);
-  var color = style.getPropertyValue("background-color");
-  color = rgbToHex(color);
-  colorObj["letter-" + letter] = color;
-}
+// for (var i = 0; i < word.length; i++) {
+//   var letter = word[i];
+//   var element = document.querySelector(".letter-" + letter);
+//   var style = window.getComputedStyle(element, null);
+//   var color = style.getPropertyValue("background-color");
+//   color = rgbToHex(color);
+//   colorObj["letter-" + letter] = color;
+// }
+
+
 export default colorObj;
